c3a5d3b6ad1cd90f4e4c8cf4a43ce2b8c1a39311
karaziyahya <karazi.yahya@gmail.com>
2021-04-13 17:20:12 +0200

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
+/*
 'use strict'; 
 (function() {
     const expr = 'Papayas';
@@ -16,3 +17,92 @@
   
 
 })();
+
+
+this.nbrPages= function nbPages(){
+    switch (True) {
+      case N <100:
+        display('Nombre de page entre 0 et 100');
+        break;
+      case N > 101 && N <200:
+        display('Nombre de page entre 101 et 200');
+        break;
+      case N > 201 && N <300:
+        display('Nombre de page entre 201 et 300');
+        break;
+      default:
+        display('Nombre de page est superieur à 300');
+        
+    }
+  
+  }
+}
+
+function Revue (numEnreg,titre,mois,annee){
+  Document.call(this,numEnreg,titre)
+  this.mois = mois; 
+  this.annee = annee;
+}
+
+function Dictionnaire (numEnreg,titre,langue){
+  Document.call(this,numEnreg,titre);
+  this.langue = langue; 
+}
+
+Document.create=Object.create(Livre.prototype);
+Document.create=Object.create(Revue.prototype);
+Document.create=Object.create(Dictionnaire.prototype);
+
+let Crep = new Livre ('59k','Crepuscule','Juan',500);
+
+
+
+*/
+
+
+
+function Person(Nom,Prenom,SalaireBrut,sexe,nbrCharge){
+  this.Nom =Nom;
+  this.Prenom =Prenom;
+  this.sexe =sexe;
+  this.SalaireBrut =SalaireBrut;
+  this.nbrCharge =nbrCharge;
+  
+
+
+function CalculSalaire(Nom,Prenom,SalaireBrut,sexe,nbrCharge,SalaireNet){
+  Person.call(this,Nom,Prenom,SalaireBrut,sexe,nbrCharge)
+  this.SalaireNet = SalaireNet;
+
+  this.Salaire = function Salaire(){
+
+  const ImpotRevenu = 18;
+  const Assurance = 7;
+  const Pension = 5;
+  const Bonus = 150;
+
+  SalaireNet=SalaireBrut*(100-ImpotRevenu-Assurance-Pension)+Bonus
+  switch (True) {
+    case  sexe===F:
+      SalaireNet+=0.02*SalaireBrut;
+      break;
+    case  N===3:
+      SalaireNet+=0.01*SalaireBrut;
+      break;
+    case  N===4:
+      SalaireNet+=0.02*SalaireBrut;
+      break;
+    default:
+      display('Nombre de page est superieur à 300');
+      
+  }
+
+}
+}
+
+
+
+let Crep = new Livre ('59k','Crepuscule','Juan',500);
+
+
+
